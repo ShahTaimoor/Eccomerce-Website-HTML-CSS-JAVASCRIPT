9feca1e207f18db94da31c165a4ed2f5898ad11e
ShahTaimoor <152506082+ShahTaimoor@users.noreply.github.com>
2024-02-25 17:35:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function openNav(){
+    document.getElementById("sidenavv").style.width = "250px";
+    document.getElementById("pagef").style.marginLeft = "250px";
+  
+
+
+}
+
+function closeBtn(){
+    document.getElementById("sidenavv").style.width = "0";
+        document.getElementById("pagef").style.marginLeft = "0";
+       
+}
+
+
+
